fedb80a2ec93fe998d77cf61beb839cf8c488e81
TheRates <raphael.tonon2805@gmail.com>
2026-04-01 16:12:16 +0200

    Remove obsolete classes and resources related to order management
    - Deleted DbConfig.java, Order.java, OrderLine.java, OrderStatus.java, OrderRepository.java, MainFrame.java, MaterialDarkTheme.java, OrderTableModel.java.
    - Removed db.properties configuration file.
    - Cleaned up compiled class files and JAR artifacts.

--- Lot 5/order-status-client/App.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-cd /d "e:\xampp\htdocs\projets\PizApp\Lot 5\order-status-client"
-call mvn clean compile exec:java
-pause
--- Lot 5/order-status-client/App.sh
+++
@@ -1,2 +0,0 @@
-cd "e:\xampp\htdocs\projets\PizApp\Lot 5\order-status-client"
-mvn clean compile exec:java
